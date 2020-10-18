57edf92bfc89747c7e2145a628375e9456fe6038
Kap Thang <thangk@uwindsor.ca>
2020-10-18 04:39:03 -0400

    3rd commit

--- test.c
+++ test.c
@@ -5,7 +5,8 @@ int main(void) {
     int age;
     printf("Enter age:");
     scanf("%d", &age);
-    printf("Hello World, test2.");
-    printf("Hello World, another one.");
+    printf("Hello World, test2.\n");
+    printf("Hello World, another one.\n");5
+    printf("Okay.");
     return 0;
 }
